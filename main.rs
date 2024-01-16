72d6a317b54f69b511a2d0ed355af727f5349704
Calvinbullock <calvinnbullock@gmail.com>
2024-01-16 15:14:04 -0700

    major refactor for console_input methods in main.rs

--- main.rs
+++ main.rs
@@ -12,24 +12,27 @@ fn main() {
     */
 
     println!("Would you like to encrypt(1) or decrypt(2) a file: ");
-    let action = console_input(true);
+    let action = console_input_int(); // true will return int
+    let file_path;
+        
+    if action == 1 { //encrypt
+        println!("Please enter the path to the file you would like to encrypt: ");
+        file_path = console_input_string();
 
-    let file_path = console_input(false);
+    } else { // decrypt
+        println!("Please enter the path to the file you would like to decrypt: ");
+        file_path = console_input_string();
 
-    // println!("{}, ", action);
-    // println!("{}, ", file_path);
+    }
+}
+
+fn file_open(file_path=String){
     
+
 }
 
-// allows me to return ether an int or a string from console_input
-enum ResultValue{
-    IntResult(u32),
-    StringResult(String),
-}
-
-fn console_input(is_int: bool) -> ResultValue{
-    // fn parses input from the terminal.
-    //      if you exspect a string set is_int false, if you want an int enter true.
+fn console_input_int() -> u32{
+    // fn parses intager input from the terminal.
 
     // I got some of this code from:
     //      https://stackoverflow.com/questions/30355185/how-to-read-an-integer-input-from-the-user-in-rust-1-0
@@ -41,22 +44,33 @@ fn console_input(is_int: bool) -> ResultValue{
     io::stdin()
         .read_line(&mut input_text)
         .expect("failed to read from stdin");
+ 
+    let mut user_choice: u32 = 0; 
+    
+    // parse the string to an int
+    let trimmed = input_text.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => {user_choice = i},
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
 
-    if is_int {  
-        let mut user_choice: u32 = 0; 
-        
-        // parse the string to an int
-        let trimmed = input_text.trim();
-        match trimmed.parse::<u32>() {
-            Ok(i) => {user_choice = i},
-            Err(..) => println!("this was not an integer: {}", trimmed),
-        };
+    return user_choice;
+}
 
-        ResultValue::IntResult(user_choice)
+fn console_input_string() -> String{
+    // fn parses string input from the terminal.
 
-    } else {
-        ResultValue::StringResult(input_text)
+    // I got some of this code from:
+    //      https://stackoverflow.com/questions/30355185/how-to-read-an-integer-input-from-the-user-in-rust-1-0
+    //      however I put it into a function and modifyied it to work 
+    //      with strings or ints and just return the i if valid I also added the enum.
+    
+    // take user input as a string
+    let mut input_text = String::new();
 
-    }
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
 
+    return input_text;
 }
