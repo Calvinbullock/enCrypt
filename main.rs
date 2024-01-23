32b62f61691e30fb17dbbf1c7901101cbfe935ef
Calvinbullock <calvinnbullock@gmail.com>
2024-01-23 12:10:08 -0700

    comment clean up

--- main.rs
+++ main.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::env;
 
 fn main() {
-    /*
+    /* TODO
     - ask if user is decrypting or encrypting a file
     - ask for a file path
     - open a file
@@ -46,7 +46,7 @@ fn console_input_int() -> u32{
 
     // I got some of this code from:
     //      https://stackoverflow.com/questions/30355185/how-to-read-an-integer-input-from-the-user-in-rust-1-0
-    //      however I put it into a function and modifyied it to pass the i value out to teh return value.
+    //      however I put it into a function and modifyied it to pass the i value out to the return value.
     
     // take user input as a string
     let mut input_text = String::new();
@@ -71,7 +71,7 @@ fn console_input_string() -> String{
 
     // I got some of this code from:
     //      https://stackoverflow.com/questions/30355185/how-to-read-an-integer-input-from-the-user-in-rust-1-0
-    //      however I put it into a function and modifyied it to pass the i value out to teh return value.
+    //      however I put it into a function and modifyied it to pass the i value out to the return value.
     
     // take user input as a string
     let mut input_text = String::new();
